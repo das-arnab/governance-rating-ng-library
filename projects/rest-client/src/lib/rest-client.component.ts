c8adb10414e4fa2d360aaf5459becf298a550ed9
Arnab Das <Arnab.Das@wu.com>
2020-01-18 18:56:31 +0530

    Added rest-client library

---
+++ projects/rest-client/src/lib/rest-client.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-RestClient',
+  template: `
+    <p>
+      rest-client works!
+    </p>
+  `,
+  styles: []
+})
+export class RestClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
